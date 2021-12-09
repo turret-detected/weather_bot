e0b090c8634f36dea757388fe9889b1c8e8b7645
turret-detected <andrew.mohnkern@gmail.com>
2021-12-08 22:25:13 -0500

    Sync

--- weather_main.py
+++ weather_main.py
@@ -19,25 +19,29 @@ api_key = "appid=61d761fc40f988a6549265538987c0b5"
 req_para = '&'.join([api_loc, api_key, api_unit])
 req_url = api_url+req_para
 
-
-
-
-# WAIT FOR REQUEST
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-ser.flush()
-
-while True:
-	if ser.in_waiting > 0:
-		line = ser.readline().decode('utf-8').rstrip()
-		if line == "req_weather":
-			print("Received request")
-			print("Replying")
-			ser.write(get_temperature().encode("utf-8"))
-			# do the thing
-
-
+# TEMPERATURE
 def get_temperature():
 	response = requests.post(req_url)
 	data = response.json()
 	temperature = data['list'][0]['main']['temp'] # in degrees F
 	return temperature
+
+
+# WAIT FOR REQUEST
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.flush()
+print("Weather python script start")
+
+while True:
+	if ser.in_waiting > 0:
+		line = ser.readline().decode('utf-8').rstrip()
+		print("Received request")
+		print(line)
+		if line == "req_weather":
+			#print("Received request")
+			print("Replying")
+			ser.write(str(get_temperature()).encode("utf-8"))
+			# do the thing
+
+
+
